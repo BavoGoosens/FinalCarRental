d32e63be16323ba36b7c5b5cdebe2b8c544b17ee
Bavo Goosens <bavo.goosens@gmail.com>
2014-10-21 11:07:34 +0200

    Gegeven Code

---
+++ CarRental-Client/client/Client.java
@@ -0,0 +1,61 @@
+package client;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Team Loco on 21/10/2014.
+ */
+public class Client implements AbstractScriptedTripTest <ReservationSession, ManagerSession> {
+
+    @Override
+    protected ReservationSession getNewReservationSession(String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected ManagerSession getNewManagerSession(String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected void checkForAvailableCarTypes(ReservationSession reservationSession, Date start, Date end) throws Exception {
+
+    }
+
+    @Override
+    protected String getCheapestCarType(ReservationSession reservationSession, Date start, Date end) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected void addQuoteToSession(ReservationSession reservationSession, Date start, Date end, String carType, String carRentalName) throws Exception {
+
+    }
+
+    @Override
+    protected List<Reservation> confirmQuotes(ReservationSession reservationSession) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected int getNumberOfReservationsBy(ManagerSession ms, String clientName) throws Exception {
+        return 0;
+    }
+
+    @Override
+    protected Set<String> getBestClients(ManagerSession ms) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected int getNumberOfReservationsForCarType(ManagerSession ms, String carRentalCompanyName, String carType) throws Exception {
+        return 0;
+    }
+
+    @Override
+    protected CarType getMostPopularCarTypeIn(ManagerSession ms, String carRentalCompanyName) throws Exception {
+        return null;
+    }
+}
---
+++ CarRental-Client/client/RunClient.java
@@ -0,0 +1,12 @@
+package client;
+
+/**
+ * Created by Team Loco on 21/10/2014.
+ */
+public class RunClient {
+
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.run();
+    }
+}
