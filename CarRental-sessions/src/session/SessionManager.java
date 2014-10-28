6fc68adc99a793c8d96f28130b1eade6a8e3f36f
Michiel Vandendriessche <michielvdd@me.com>
2014-10-28 13:28:32 +0100

    IRental fix

--- CarRental-sessions/src/session/SessionManager.java
+++ CarRental-sessions/src/session/SessionManager.java
@@ -2,18 +2,15 @@ package session;
 
 
 import naming.NamingServiceRemote;
-import rental.IRentalServer;
 
 /**
  * Created by Team Loco on 22/10/2014.
  */
 public class SessionManager implements SessionManagerRemote {
 
-    private IRentalServer rentalServer;
     private NamingServiceRemote namingService;
 
-    public  SessionManager(IRentalServer rentalServer, NamingServiceRemote namingService){
-        this.rentalServer = rentalServer;
+    public  SessionManager(NamingServiceRemote namingService){
         this.namingService = namingService;
     }
 
